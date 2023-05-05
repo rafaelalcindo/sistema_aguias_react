ea98817e22261e0f174bb187bf30386cb4e387df
Rafael Alcindo <rafael_alcindo@hotmail.com>
2023-05-05 18:06:58 -0300

    ajustes no checkbox

--- src/components/CheckListSelect/CheckListSelectEventos/index.tsx
+++ src/components/CheckListSelect/CheckListSelectEventos/index.tsx
@@ -7,6 +7,8 @@ import { ListaProps } from '../../../types/ListaProps';
 
 import styles from './styles.module.scss';
 import { Context } from '../../../context/AuthContext';
+import Swal from 'sweetalert2';
+import { object } from 'yup';
 
 
 interface CheckListSelectEventos {
@@ -24,7 +26,9 @@ export function CheckListSelectEventos(
 ) {
 
     const [usuarios, setUsuarios] = useState<UsuarioProps[]>([]);
+    const [usuariosRecebePontos, setUsuariosRecebePontos] = useState<UsuarioProps[]>([]);
     const [selectedUsuarios, setSelectedUsuarios] = useState<string[]>([]);
+
 
     const { handleLogOut, usuario } = useContext(Context);
 
@@ -43,6 +47,19 @@ export function CheckListSelectEventos(
         getUsuarios();
 
     }, [eventoId]);
+
+    /**
+     * Serve para deixar todos os Checks falsos após a confirmação
+     */
+    function checkAllFalse() {
+        var inputs = document.getElementsByTagName('input');
+
+        for (var i = 0; i < inputs.length; i++) {
+            if (inputs[i].type == 'checkbox') {
+                inputs[i].checked = false;
+            }
+        }
+    }
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { value, checked } = e.target;
@@ -75,7 +92,49 @@ export function CheckListSelectEventos(
         // console.log(value, checked);
     }
 
-    console.log(selectedUsuarios);
+    async function passSelectedToPointSide() {
+        let objectToSet = [] as UsuarioProps[];
+        let newUsuarioList = [] as UsuarioProps[];
+
+        if (selectedUsuarios.length > 0) {
+            objectToSet = usuarios.filter((register) => {
+                let filterResu = selectedUsuarios.filter((selected) => {
+                    if (selected == register.id)
+                        return register;
+                });
+
+                return (filterResu.length > 0);
+            });
+
+            newUsuarioList = usuarios.filter((register) => {
+                if (!objectToSet.includes(register))
+                    return register;
+            })
+        } else {
+            Swal.fire({
+                title: 'Error!',
+                text: 'Por favor, selecione pelo menos 1 desbravador',
+                icon: 'error',
+                confirmButtonText: 'OK'
+            });
+        }
+
+        let newArraySelected = [
+            ...usuariosRecebePontos,
+            ...objectToSet
+        ];
+
+        setUsuariosRecebePontos(
+            [
+                ... new Set(newArraySelected)
+            ]
+        );
+
+        setUsuarios(newUsuarioList);
+
+        checkAllFalse();
+
+    }
 
     return (
         <div>
@@ -86,8 +145,8 @@ export function CheckListSelectEventos(
                         usuarios ?
                             usuarios.map((item, index) => (
                                 <div className={`${styles.inputGroup}`} key={index} >
-                                    <input id={`option${index}`} name={`option${index}`} value={item.id} type="checkbox" onChange={handleChange} />
-                                    <label htmlFor={`option${index}`}>{item.nome} {item.sobrenome}</label>
+                                    <input id={`option${item.id}`} name={`option${item.id}`} value={item.id} type="checkbox" onChange={handleChange} />
+                                    <label htmlFor={`option${item.id}`}>{item.nome} {item.sobrenome}</label>
                                 </div>
                             ))
                             :
@@ -106,7 +165,17 @@ export function CheckListSelectEventos(
                 &nbsp;
                 &nbsp;
                 <div className={`${styles.col_list} shadow-lg shadow-indigo-500/40`}>
-                    <h2>Teste 2</h2>
+                    {
+                        usuariosRecebePontos ?
+                            usuariosRecebePontos.map((item, index) => (
+                                <div className={`${styles.inputGroup}`} key={index} >
+                                    <input id={`optionSelected${item.id}`} name={`optionSelected${item.id}`} value={item.id} type="checkbox" onChange={handleChange} />
+                                    <label className='CheckboxListSelectd' htmlFor={`optionSelected${item.id}`}>{item.nome} {item.sobrenome}</label>
+                                </div>
+                            ))
+                            :
+                            ''
+                    }
                 </div>
 
             </div>
@@ -115,7 +184,7 @@ export function CheckListSelectEventos(
                 <button
                     type="button"
                     className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-
+                    onClick={passSelectedToPointSide}
                 >
                     adicionar
                 </button>
